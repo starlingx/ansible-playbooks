openstack-archive/stx-config

Ansible Bootstrap Deployment
This commit is initial submission of bootstrap playbook which
enables the bootstrap of initial controller. The playbook
defaults are meant for configuring the localhost in vbox
development environment. Custom hosts file and user overrides
are required for configuring multiple hosts and lab specific setup.
Secret file and SSH keys are required for production test enviroment.

Tests performed:
 - installation
 - config_controller complete to ensure the current method of
   configuring the first controller is intact
 - localhost bootstrap with default hosts file
 - multiple remote hosts bootstrap with custom hosts file
 - reconfigurations with user overrides
 - AIOSX unlock (a custom build of maintenance agent is required.
   A long-term solution is in the works.)
 - Failure & skip play cases (invalid config inputs, incorrect load,
   connection failure, no changes replay, etc...)

TODO:
 - Maintenance update for bootstrap playbook
 - AIODX and Standard system tests
 - Port recent changes to config_controller to Playbook
 - Docker proxy/custom registry related tests
 - Package bootstrap playbook in SDK
 - Config_controller cleanup

Story: 200476
Task: 29686
Task: 29687

Change-Id: If553f1eeed32606bacc690ef277e60606e9d93ea
Signed-off-by: Tee Ngo <tee.ngo@windriver.com>

playbookconfig/centos/playbookconfig.spec
@@ -1,0 +1,48 @@
+Name: playbookconfig
+Version: 1.0
+Release: %{tis_patch_ver}%{?_tis_dist}
+Summary: Ansible Playbooks for StarlingX Configuration
+
+Group: base
+License: Apache-2.0	
+URL: unknown
+Source0: %{name}-%{version}.tar.gz	
+
+Requires: python
+Requires: python-netaddr
+Requires: sshpass
+Requires: python2-ptyprocess
+Requires: python2-pexpect
+Requires: ansible
+
+%description
+This packages contains playbooks used for configuring StarlingX.
+
+%define local_stx_ansible_dir %{_datadir}/ansible/stx-ansible
+%define local_etc_ansible /etc/ansible
+%define debug_package %{nil}
+
+%prep
+%setup -q
+
+%build
+
+%install
+make install DESTDIR=%{buildroot}%{local_stx_ansible_dir}
+
+%post
+#if [ $1 -eq 1 ] ; then
+  #cp -f %{local_stx_ansible_dir}/playbooks/bootstrap/ansible.cfg %{local_etc_ansible}
+  #cp -f %{local_stx_ansible_dir}/playbooks/bootstrap/hosts %{local_etc_ansible}
+mv %{local_stx_ansible_dir}/playbooks/bootstrap/ansible.cfg %{local_etc_ansible}
+mv %{local_stx_ansible_dir}/playbooks/bootstrap/hosts %{local_etc_ansible}
+chmod 644 %{local_etc_ansible}/ansible.cfg
+chmod 644 %{local_etc_ansible}/hosts
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%defattr(-,root,root,-)
+%doc LICENSE
+%{local_stx_ansible_dir}/*

starlingx/ansible-playbooks

 - stx-application applied in AIOSX and AIODX
 - Support for standard & storage configurations
Co-Authored-By: Ovidiu Poncea <ovidiu.poncea@windriver.com>

playbookconfig/centos/playbookconfig.spec
@@ -1,0 +1,45 @@
+Name: playbookconfig
+Version: 1.0
+Release: %{tis_patch_ver}%{?_tis_dist}
+Summary: Ansible Playbooks for StarlingX Configurations
+
+Group: base
+License: Apache-2.0
+URL: unknown
+Source0: %{name}-%{version}.tar.gz
+
+Requires: python
+Requires: python-netaddr
+Requires: sshpass
+Requires: python2-ptyprocess
+Requires: python2-pexpect
+Requires: ansible
+
+%description
+This package contains playbooks used for configuring StarlingX.
+
+%define local_stx_ansible_dir %{_datadir}/ansible/stx-ansible
+%define local_etc_ansible /etc/ansible
+%define debug_package %{nil}
+
+%prep
+%setup -q
+
+%build
+
+%install
+make install DESTDIR=%{buildroot}%{local_stx_ansible_dir}
+
+%post
+cp %{local_stx_ansible_dir}/playbooks/bootstrap/ansible.cfg %{local_etc_ansible}
+cp %{local_stx_ansible_dir}/playbooks/bootstrap/hosts %{local_etc_ansible}
+chmod 644 %{local_etc_ansible}/ansible.cfg
+chmod 644 %{local_etc_ansible}/hosts
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%defattr(-,root,root,-)
+%doc LICENSE
+%{local_stx_ansible_dir}/*