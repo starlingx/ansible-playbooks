04a8e8abe58803e78ce58ad287adde27bfc5f8c1
Daian Cardoso Sganderlla <Daian.CardosoSganderlla@windriver.com>
2022-04-07 15:11:40 -0400

    Add Ceph's mpath partition types
    Today StarlingX just checks for regular partition type while checking
    some partitions. Because of that, when using a mpath disk there are
    some cases that StarlingX will treat these partition as standard
    partitions, instead of Ceph's partitions.
    
    Test Plan:
    
    PASS: Install AIO-SX
    PASS: Install AIO-SX with one of the disks previously configured as an
    OSD
    
    Story: 2010046
    Task: 45423
    
    Signed-off-by: Daian Cardoso Sganderlla <Daian.CardosoSganderlla@windriver.com>
    Change-Id: I0e007274e7321f530af9ba5a5bae62e55a91a185

--- playbookconfig/src/playbooks/roles/bootstrap/validate-config/files/wipe_osds.sh
+++ playbookconfig/src/playbooks/roles/bootstrap/validate-config/files/wipe_osds.sh
@@ -16,27 +16,18 @@
 sleep 2
 
 part_type_guid_str="Partition GUID code"
-CEPH_OSD_GUID="4FBD7E29-9D25-41B8-AFD0-062C0CEFF05D"
-CEPH_JOURNAL_GUID="45B0969E-9B03-4F30-B4C6-B4B80CEFF106"
+CEPH_REGULAR_OSD_GUID="4FBD7E29-9D25-41B8-AFD0-062C0CEFF05D"
+CEPH_REGULAR_JOURNAL_GUID="45B0969E-9B03-4F30-B4C6-B4B80CEFF106"
+CEPH_MPATH_OSD_GUID="4FBD7E29-8AE0-4982-BF9D-5A8D867AF560"
+CEPH_MPATH_JOURNAL_GUID="45B0969E-8AE0-4982-BF9D-5A8D867AF560"
 
 # This script is not supposed to fail, print extended logs from ansible if it does.
 set -x
 
-for f in /dev/disk/by-path/*; do
-
-    # list of partitions in the loop may no longer be valid as
-    # we are wiping disks.
-    if [ ! -e "${f}" ]; then
-        continue
-    fi
-
-    dev=$(readlink -f $f)
-    lsblk --nodeps --pairs $dev | grep -q 'TYPE="disk"'
-    if [ $? -ne 0 ] ; then
-        continue
-    fi
-
-    set -e
+function wipe_if_ceph_disk {
+    local dev=$1
+    local osd_guid=$2
+    local journal_guid=$3
 
     part_no=1
     ceph_disk="false"
@@ -49,7 +40,7 @@ for f in /dev/disk/by-path/*; do
         sgdisk_part_info=$(flock ${dev} sgdisk -i ${part_no} ${dev} || true)
         guid=$(echo "${sgdisk_part_info}" | \
             grep "$part_type_guid_str" | awk '{print $4;}')
-        if [ "${guid}" == "$CEPH_OSD_GUID" ]; then
+        if [ "${guid}" == "$osd_guid" ]; then
             echo "Found Ceph OSD partition #${part_no} ${part}, erasing!"
             dd if=/dev/zero of=${part} bs=512 count=34 2>/dev/null
             seek_end=$((`blockdev --getsz ${part}` - 34))
@@ -57,7 +48,7 @@ for f in /dev/disk/by-path/*; do
                 bs=512 count=34 seek=${seek_end} 2>/dev/null
             parted -s ${dev} rm ${part_no}
             ceph_disk="true"
-        elif [ "${guid}" == "$CEPH_JOURNAL_GUID" ]; then
+        elif [ "${guid}" == "$journal_guid" ]; then
             echo "Found Ceph journal partition #${part_no} ${part}, erasing!"
             dd if=/dev/zero of=${part} bs=1M count=100 2>/dev/null
             seek_end=$((`blockdev --getsz ${part}` / (1024 * 2) - 100 ))
@@ -76,5 +67,58 @@ for f in /dev/disk/by-path/*; do
         wipefs -a ${dev}
     fi
 
+}
+
+# Only non multipath
+for f in /dev/disk/by-path/*; do
+
+    # list of partitions in the loop may no longer be valid as
+    # we are wiping disks.
+    if [ ! -e "${f}" ]; then
+        continue
+    fi
+
+    dev=$(readlink -f $f)
+    lsblk --nodeps --pairs $dev | grep -q 'TYPE="disk"'
+    if [ $? -ne 0 ] ; then
+        continue
+    fi
+
+    #Skip if this is a valid multipath device
+    multipath -c $dev
+    if [ $? -eq 0 ] ; then
+        continue
+    fi
+
+    set -e
+
+    wipe_if_ceph_disk $dev $CEPH_REGULAR_OSD_GUID $CEPH_REGULAR_JOURNAL_GUID
+
     set +e
 done
+
+# Only multipath
+for f in /dev/disk/by-id/dm-uuid*; do
+
+    # list of partitions in the loop may no longer be valid as
+    # we are wiping disks.
+    if [ ! -e "${f}" ]; then
+        continue
+    fi
+
+    dm_path=$(readlink -f $f)
+
+    lsblk --nodeps --pairs $dm_path | grep -q 'TYPE="mpath"'
+    if [ $? -ne 0 ] ; then
+        continue
+    fi
+
+    dev=$(find -L /dev/mapper -samefile $dm_path)
+
+    set -e
+
+    wipe_if_ceph_disk $dev $CEPH_MPATH_OSD_GUID $CEPH_MPATH_JOURNAL_GUID
+
+    set +e
+done
+
