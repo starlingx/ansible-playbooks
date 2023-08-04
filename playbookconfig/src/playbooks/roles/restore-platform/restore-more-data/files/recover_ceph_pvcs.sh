8bd52840c9c2e79ea50fcef58d6b7732e3579f20
Felipe Sanches Zanoni <Felipe.SanchesZanoni@windriver.com>
2023-08-04 15:23:00 -0300

    Recreate PVCs when restoring backup with wipe_ceph_osds=true
    When restoring from a backup with the flag wipe_ceph_osds=true, the
    recovered pods might get stuck waiting for the PVC to be attached and
    mounted, but the ceph disks were wiped.
    
    To recover from this situtation, a script is added to iterate through
    each namespace checking for PVCs that were provisioned by ceph
    provisioners. When a broken PVC is found, it is deleted and reapplied
    to be reprovisioned. Then the Pods get back running.
    
    Test Plan:
      PASS: AIO-SX Optimized B&R with helm charts using pvcs applied
      PASS: AIO-SX Legacy B&R with helm charts using pvcs applied
      PASS: Standard with Storage nodes B&R with helm charts using pvcs
            applied
      PASS: AIO-SX subcloud Optimized B&R with wipe_ceph_osds=true
    
    Closes-bug: 2030120
    
    Change-Id: I2a0a15c526689ec30eea514933a6d1eabebf23db
    Signed-off-by: Felipe Sanches Zanoni <Felipe.SanchesZanoni@windriver.com>

---
+++ playbookconfig/src/playbooks/roles/restore-platform/restore-more-data/files/recover_ceph_pvcs.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright (c) 2023 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Temporary file for manipulation
+PVC_RECOVER_YAML="/tmp/pvc-recover.yaml"
+
+# Get all namespaces to search for PVCs
+PVCS_NAMESPACE_LIST=$(kubectl get namespaces -o name | sed 's/^namespace\///')
+
+# Iterate among all namespaces
+for NAMESPACE in ${PVCS_NAMESPACE_LIST}; do
+    echo "Checking namespace '${NAMESPACE}'"
+
+    # Get all PVCs
+    PVCS_NAME_LIST=$(kubectl -n "${NAMESPACE}" get pvc -o name)
+    if [ -z "${PVCS_NAME_LIST}" ]; then
+        echo "  No PVCs found on namespace '${NAMESPACE}'"
+        continue
+    fi
+
+    # Delete all PVCs in NAMESPACE
+    for PVC in ${PVCS_NAME_LIST}; do
+
+        # Store PVC details
+        kubectl -n "${NAMESPACE}" get "${PVC}" -o yaml > ${PVC_RECOVER_YAML}
+        if ! grep -E "rbd.csi.ceph.com|cephfs.csi.ceph.com|ceph.com/rbd|ceph.com/cephfs" \
+                ${PVC_RECOVER_YAML} 1>/dev/null 2>&1; then
+            echo "  Skipping ${PVC}: not provisioned by ceph-csi"
+            continue
+        fi
+
+        # Remove bind information
+        sed -i -e '/volumeName:/d' -e '/pv.kubernetes.io\/bind-completed:/d' \
+            -e '/pv.kubernetes.io\/bound-by-controller:/d' "${PVC_RECOVER_YAML}"
+
+        # Delete the PVC
+        kubectl -n "${NAMESPACE}" delete "${PVC}" --wait=false
+        kubectl -n "${NAMESPACE}" patch "${PVC}" \
+                -p '{"metadata":{"finalizers":null}}' --type=merge
+
+        # Check if it has been deleted
+        for RETRY in {1..15}; do
+            if ! kubectl -n "${NAMESPACE}" get "${PVC}" 1>/dev/null 2>&1; then
+                break
+            fi
+            sleep 1
+        done
+        if [ $RETRY -eq 15 ]; then
+            echo "  Could not delete ${PVC}"
+        fi
+
+        # Recreate the PVC
+        kubectl apply -f "${PVC_RECOVER_YAML}"
+        rm -f "${PVC_RECOVER_YAML}"
+    done
+done
