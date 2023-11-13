570aa9ea12e05de46e73b809e917faa95318a3cd
Bruce Jones <bruce.jones@windriver.com>
2023-11-13 19:23:57 +0000

    Revert "Reduce keyring access to once per playbook run."
    This reverts commit 86f739f0eb886d51842538c287e132e34c302863.
    
    Reason for revert: breaks sub-cloud pre-staging.
    
    Change-Id: Iec5d412dcb7b1d79ed216c1d27e76edb9d68eb3b

--- playbookconfig/src/playbooks/roles/common/push-docker-images/files/download_images.py
+++ playbookconfig/src/playbooks/roles/common/push-docker-images/files/download_images.py
@@ -6,12 +6,12 @@
 #
 
 import docker
-import json
-import keyring
-import os
-import subprocess
 import sys
 import time
+import os
+import json
+import keyring
+import subprocess
 
 from random import SystemRandom
 
@@ -44,48 +44,30 @@ add_docker_prefix = False
 
 
 def get_local_registry_auth():
-    """Obtain local registry credentials."""
-    password = None
-
-    # If, in the off-chance that keyring itself throws an excption, catch
-    # and print out.
-    try:
-        password = keyring.get_password("sysinv", "services")
-    except Exception as error:
-        print("download_images.py: get_local_registry_auth - Unable to access keyring.")
-        print(str(error))
-
+    password = keyring.get_password("sysinv", "services")
     if not password:
         raise Exception("Local registry password not found.")
-
     return dict(username="sysinv", password=str(password))
 
 
 def convert_img_for_local_lookup(img):
-    """Convert Image for Local Lookup.
-
-    This function converts the given image reference to the
-    format that is suitable for lookup or push to the
-    local registry.
-
-    Examples of passed img reference:
-      - k8s.gcr.io/kube-proxy:v.16.0
-      - privateregistry.io:5000/kube-proxy:v1.16.0
-
-    To push to local registry, local registry url
-    'registry.local:9001' needs to be prepended to
-    image reference. The registry url of passed img
-    may contains a port, if it has a port, we strip
-    it out as Docker does not allow the format of
-    the image that has port in repositories/namespaces
-    i.e.
-    Invalid format:
-      registry.local:9001/privateregistry.io:5000/kube-proxy:v1.16.0
-
-    Args:
-        img: (str) - String imagag format
-
-    """
+    # This function converts the given image reference to the
+    # format that is suitable for lookup or push to the
+    # local registry.
+    #
+    # Examples of passed img reference:
+    #  - k8s.gcr.io/kube-proxy:v.16.0
+    #  - privateregistry.io:5000/kube-proxy:v1.16.0
+    #
+    # To push to local registry, local registry url
+    # 'registry.local:9001' needs to be prepended to
+    # image reference. The registry url of passed img
+    # may contains a port, if it has a port, we strip
+    # it out as Docker does not allow the format of
+    # the image that has port in repositories/namespaces
+    # i.e.
+    # Invalid format:
+    #   registry.local:9001/privateregistry.io:5000/kube-proxy:v1.16.0
     if img.find('/') > 0:
         registry_url = img[:img.find('/')]
         if ':' in registry_url:
@@ -106,18 +88,9 @@ def convert_img_for_local_lookup(img):
 
 
 def handle_docker_exception(ex, err_msg, image):
-    """Handle Docker Exception.
-
-    Credentials or disk space related failures result in hard exit. Other
-    types of error result in soft exit. The Ansible task that calls this
-    script will issue a retry.
-
-    Args:
-        ex: (Exception) - This is the caught exception class.
-        err_msg: (str) - String error message.
-        image (str) - Associated Image string.
-
-    """
+    # Credentials or disk space related failures result in hard exit. Other
+    # types of error result in soft exit. The Ansible task that calls this
+    # script will issue a retry.
     if (isinstance(ex, docker.errors.NotFound) or
             any(err in str(ex) for err in HARD_FAIL_ERRORS)):
         print(" HARD FAIL -" + err_msg + str(ex))
@@ -129,14 +102,7 @@ def handle_docker_exception(ex, err_msg, image):
 
 
 def get_img_tag_with_registry(pub_img):
-    """Obtain image tag.
-
-    This function returns image tag suitable for downloading
-
-    Args:
-        pub_img: (str) - Input image string to be
-        converted.
-    """
+    # This function returns image tag suitable for downloading
     if registries == DEFAULT_REGISTRIES:
         # return as no private registires configured
         return pub_img
@@ -150,18 +116,11 @@ def get_img_tag_with_registry(pub_img):
 
 
 def get_image_list_with_auth_info(images):
-    """Reformat image list into tuples.
-
-    This function returns a list of image tuples. Each tuple contains
-    3 elements:
-       a) image tag suitable for prestaging
-       b) image tag suitable for downloading
-       c) image auth info
-
-    Args:
-        images: list - List of image strings.
-
-    """
+    # This function returns a list of image tuples. Each tuple contains
+    # 3 elements:
+    #    a) image tag suitable for prestaging
+    #    b) image tag suitable for downloading
+    #    c) image auth info
     images_with_auth = []
     for img in images:
         registry_auth = None
@@ -195,19 +154,15 @@ def get_image_list_with_auth_info(images):
     return images_with_auth
 
 
-def download_and_push_an_image(img, auth):
-    """Download and push the image.
-
-    Args:
-        img: (str) - Image string.
-        auth: (dict) - Dictionary of local registry credentials.
-
-    """
+def download_and_push_an_image(img):
+    # This function is used to pull an image from public/private
+    # registry and push it to the local registry.
     local_img = convert_img_for_local_lookup(img)
     target_img = get_img_tag_with_registry(img)
     err_msg = " Image download failed: %s " % target_img
-    client = docker.APIClient()
 
+    client = docker.APIClient()
+    auth = get_local_registry_auth()
     try:
         client.inspect_distribution(local_img, auth_config=auth)
         print("Image {} found on local registry".format(target_img))
@@ -256,22 +211,16 @@ def download_and_push_an_image(img, auth):
 
 
 # TODO(tngo): Remove this function post StarlingX 9.0
-def download_a_local_image(img, auth):
-    """Download a local image.
-
-    This function is used to pull the specified image from the local
-    registry for image prestage on CentOS.
-
-    Args:
-        img: (str) - Image string.
-        auth: (dict) - Dictionary of local registry credentials.
-
-    """
+def download_a_local_image(img):
+    # This function is used to pull the specified image from the local
+    # registry for image prestage on CentOS.
     err_msg = " Image retrieval failed: %s " % img
+
     local_img = LOCAL_REGISTRY_URL + img
 
     try:
         client = docker.APIClient()
+        auth = get_local_registry_auth()
         for line in client.pull(local_img, auth_config=auth, stream=True):
             j = json.loads(line)
             if 'errorDetail' in j:
@@ -286,28 +235,24 @@ def download_a_local_image(img, auth):
 
 
 # TODO(tngo): Remove this function post StarlingX 9.0
-def download_an_image(img_tuple, local_auth):
-    """Download image.
+def download_an_image(img_tuple):
+    # This function is used to download an image from the public/private
+    # registry for image prestage on CentOS. It first checks if the image
+    # is already available in the local registry and pulls it from there.
+    # Otherwise, it pulls the image from the specified source.
 
-    This function is used to download an image from the public/private
-    registry for image prestage on CentOS. It first checks if the image
-    is already available in the local registry and pulls it from there.
-    Otherwise, it pulls the image from the specified source.
-
-    Args:
-        image_tuple: (tuple) - Tuple containing image details.
-        local_auth: (dict) - Dictionary of local registry credentials.
-
-    """
     prestage_img, target_img, registry_auth = img_tuple
     local_img = convert_img_for_local_lookup(prestage_img)
-    client = docker.APIClient()
 
     # Leave this is for debugging
     # print("prestage_img: %s, target_img: %s, local_img: %s, auth_info: %s" %
     #       (prestage_img, target_img, local_img, registry_auth))
+
+    client = docker.APIClient()
+    local_auth = get_local_registry_auth()
     try:
         err_msg = " Image download failed: %s " % target_img
+
         client.inspect_distribution(local_img, auth_config=local_auth)
         print("Image {} found on local registry".format(target_img))
         return download_a_local_image(local_img.split(LOCAL_REGISTRY_URL)[1])
@@ -325,23 +270,17 @@ def download_an_image(img_tuple, local_auth):
             return handle_docker_exception(e, err_msg, target_img)
 
 
-def download_and_push_an_image_for_prestage(img_tuple, local_auth):
-    """Download and push image or prestage.
+def download_and_push_an_image_for_prestage(img_tuple):
+    # This function is used to download an image from the public/private
+    # registry and push it to the local registry for image prestage on
+    # Debian. It first checks if the image already exists in the local
+    # registry before pulling it from the specified source.
 
-    This function is used to download an image from the public/private
-    registry and push it to the local registry for image prestage on
-    Debian. It first checks if the image already exists in the local
-    registry before pulling it from the specified source.
-
-    Args:
-        image_tuple: (tuple) - Tuple containing image details.
-        local_auth: (dict) - Dictionary of local registry credentials.
-
-    """
     prestage_img, target_img, registry_auth = img_tuple
     local_img = convert_img_for_local_lookup(prestage_img)
-    client = docker.APIClient()
 
+    client = docker.APIClient()
+    local_auth = get_local_registry_auth()
     try:
         err_msg = " Image download failed: %s " % target_img
 
@@ -368,47 +307,17 @@ def download_and_push_an_image_for_prestage(img_tuple, local_auth):
 
 
 # TODO(tngo): Remove this function post StarlingX 9.0
-def generate_image_outfile(images, image_outfile):
-    """Generate image output file.
-
-    This function writes the list of images in docker cache that can
-    be used for prestaging to the given file.
-
-    Args:
-        images: (list) - List of image strings.
-        image_outfile: (str) - Path to output file desired.
-
-    """
+def generate_image_outfile(images, outfile):
+    # This function writes the list of images in docker cache that can
+    # be used for prestaging to the given file.
     with open(image_outfile, 'a') as f:
         for image in images:
             f.write(image + "\n")
 
 
 def map_function(images, function, local_download=False):
-    """This function multi-threads the process of obtaining images.
-
-    Args:
-        images: list of image strings.
-        function: One of:
-            download_and_push_an_image
-            download_and_push_an_image_for_prestage
-            download_a_local_image
-            download_an_image
-        local_download: True if this funtion is going to source images
-        from a local registry. False otherwise. Defaults to False.
-
-    """
     failed_images = []
     threads = min(MAX_DOWNLOAD_THREAD, len(images))
-
-    # Obtain local credentials once and pass to each job in the map.
-    local_auth = get_local_registry_auth()
-
-    # Each argument going into imap has to be a list of the same size, so if there
-    # are N images in the list, then local_auth must be repeated N times.
-    auth_list = []
-    for _ in range(0, len(images)):
-        auth_list.append(local_auth)
 
     if local_download:
         # monkey_patch is called on the eventlet to improve parallelism when
@@ -420,11 +329,10 @@ def map_function(images, function, local_download=False):
         eventlet.monkey_patch(os=False)
         from eventlet import greenpool  # noqa: E402
     else:
-        # serial download
         from eventlet import greenpool
 
     pool = greenpool.GreenPool(size=threads)
-    for image, success in pool.imap(function, images, auth_list):
+    for image, success in pool.imap(function, images):
         if not success:
             failed_images.append(image)
         elif purge_images_list_file and image is not None:
@@ -435,7 +343,6 @@ def map_function(images, function, local_download=False):
 
 
 if __name__ == '__main__':
-
     if len(sys.argv) < 2:
         raise Exception("Invalid Input!")
 
