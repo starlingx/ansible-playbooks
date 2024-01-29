57ba971e6d2b132030b8b052d81187b2fa350111
Joseph Vazhappilly <joseph.vazhappillypaily@windriver.com>
2024-01-29 08:23:00 -0500

    Update host record validation during bootstrap
    Ansible bootstrap playbook is updated to check and fail, if duplicate
    DNS host records or invalid hostnames are specified in the
    `user_dns_host_records` parameter.
    
    Closes-bug: 2051553
    
    Test Plan:
    PASS: Verify bootstrap playbook with duplicate host records
    PASS: Verify bootstrap playbook with valid & invalid hostname
    
    Change-Id: Icdfa1bb2b1866020964ef22efe5fbced10618ff8
    Signed-off-by: Joseph Vazhappilly <joseph.vazhappillypaily@windriver.com>

--- playbookconfig/src/playbooks/roles/bootstrap/validate-config/files/parse_user_dns_host_record.py
+++ playbookconfig/src/playbooks/roles/bootstrap/validate-config/files/parse_user_dns_host_record.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #
-# Copyright (c) 2023 Wind River Systems, Inc.
+# Copyright (c) 2023-2024 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,8 +9,21 @@
 
 import sys
 import yaml
+
+from sysinv.common.utils import is_valid_dns_hostname
 from sysinv.common.utils import is_valid_ipv4
 from sysinv.common.utils import is_valid_ipv6
+
+
+def check_duplicate_host_records(user_host_data):
+    host_record_list = []
+    for host, value in user_host_data.items():
+        value_set = set(x.strip() for x in value.split(','))
+        if value_set not in host_record_list:
+            host_record_list.append(value_set)
+        else:
+            msg = "User dns host-records has duplicate: %s" % (value)
+            raise ValueError(msg)
 
 
 def parse_user_dns_host_records(user_host_data):
@@ -25,12 +38,16 @@ def parse_user_dns_host_records(user_host_data):
                     ipv4 = value
                 elif is_valid_ipv6(value):
                     ipv6 = value
-                else:
+                elif is_valid_dns_hostname(value if '.' in value else value + '.dummy'):
+                    # If hostname does not contain Top-Level Domain (TLD),
+                    # append 'dummy' as TLD for the input of is_valid_dns_hostname()
                     domain_names.append(value)
+                else:
+                    raise ValueError("Invalid DNS domain name: %s" % (value))
 
         if not domain_names or not (ipv4 or ipv6):
-            raise ValueError("""User dns host-records has either null ip address
-                            or domain name for host: """ + host)
+            msg = "User dns host-records has invalid format: {%s: %s}" % (host, values)
+            raise ValueError(msg)
         if ipv4:
             ipv4_entry = " ".join([ipv4] + [str(domain_name) for domain_name in domain_names])
             print(ipv4_entry)
@@ -44,6 +61,7 @@ if __name__ == '__main__':
     user_host_data_yaml = sys.argv[1]
     user_host_data = yaml.safe_load(user_host_data_yaml)
     try:
+        check_duplicate_host_records(user_host_data)
         parse_user_dns_host_records(user_host_data)
     except ValueError as e:
         print(f"An error occurred during parsing user dns host-records: {str(e)}")
